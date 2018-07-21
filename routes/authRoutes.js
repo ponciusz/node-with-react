54d516356c3411b8be251ce372cc22d6aa9f2d10
Kamil Albrycht <ponciusz@gmail.com>
2018-07-21 22:37:56 +0100

    LogOut

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 
-module.exports = (app) => {
+module.exports = app => {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
@@ -10,7 +10,12 @@ module.exports = (app) => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
-  app.get('/api/current_user', (req, res) => {
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user)
+  });
+
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
-  })
+  });
 };
