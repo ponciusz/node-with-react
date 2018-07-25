40c375c2e2fc1eeee511fec6a757f0351ad7779a
Kamil Albrycht <ponciusz@gmail.com>
2018-07-25 19:36:38 +0100

    Action creators

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import "materialize-css/dist/css/materialize.min.css";
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 import Header from "./Header";
 
@@ -8,22 +10,25 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
- 
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <React.Fragment>
+            <Header />
+            <Route exact={true} path="/" component={Landing} />
+            <Route exact={true} path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <React.Fragment>
-          <Header />
-          <Route exact={true} path="/" component={Landing} />
-          <Route exact={true} path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </React.Fragment>
-      </BrowserRouter>
-    </div>
-  );
-};
-
-export default App;
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from 'redux-thunk';
 
 import App from "./components/App";
 import reducers from "./reducers";
@@ -10,7 +11,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducers /* preloadedState, */,
-  composeEnhancers(applyMiddleware())
+  composeEnhancers(applyMiddleware(
+    reduxThunk,
+  ))
 );
 
 ReactDOM.render(
