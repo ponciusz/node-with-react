c9f2a7abb3033ed5026a263373bcfb1e10899c13
Kamil Albrycht <ponciusz@gmail.com>
2018-07-25 15:50:01 +0100

    cra client + promises to asunc/await

--- services/passport.js
+++ services/passport.js
@@ -3,19 +3,16 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-
-
 const User = mongoose.model("users");
 
-passport.serializeUser((user, done) =>{
+passport.serializeUser((user, done) => {
   done(null, user.id);
-})
+});
 
-passport.deserializeUser((id, done)=>{
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    })
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -25,16 +22,14 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: keys.callbackUrl
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
